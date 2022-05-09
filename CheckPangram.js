95abb02b085ecff6c8f1d84cbc857cf1b4a5d7f4
PrinceMargaret <princeagrahari2000@gmail.com>
2022-05-10 00:24:11 +0530

    first commit

---
+++ CheckPangram.js
@@ -0,0 +1,20 @@
+const pangram = (str) => {
+    const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+    let strLower = str.toLowerCase();
+    let strArr = strLower.split('');
+    let alphabetArr = alphabet.split('');   
+    let result = [];
+    for (let i = 0; i < alphabetArr.length; i++) {
+        if (strArr.includes(alphabetArr[i])) {
+            result.push(alphabetArr[i]);
+        }
+    }
+    if (result.length === 26) {
+        return 1;
+    } else {
+        return 0;
+    }
+
+}
+
+console.log(pangram('The quick brown fox jumps over the lazy dog'));
